ea82c06cfb13a03d721810bfa749505e1066de95
Marek Kapolka <marek.kapolka@gmail.com>
2021-03-30 09:09:17 -0700

    main.py initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+import argparse
+
+def extract_zaubers(file):
+    pass
+
+def parse_directory(path):
+    pass
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--directory", "-d", type=str)
+    print("hi")
